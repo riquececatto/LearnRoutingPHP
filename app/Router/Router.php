f53245c0cd6f63824a2023e2efbffc334a202b0b
Henrique Cecatto <rique.cecatto00@gmail.com>
2022-12-28 02:47:51 -0300

    Added index.php and master.php views, constants.php helper function and also added Controller.php and Router.php classes

---
+++ app/Router/Router.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Router;
+
+class Rotuer 
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+    require_once '../vendor/autoload.php';
+
+
+} catch(\Exception $e) {
+    var_dump($e->getMessage());
+}
