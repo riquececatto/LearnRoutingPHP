73ea38c28f8e92b639dcea5555e568c3ab881b76
Henrique Cecatto <rique.cecatto00@gmail.com>
2022-12-29 20:05:35 -0300

    Getting the getRouter and send for the master view

--- app/views/master.php
+++ app/views/master.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MAIN</title>
+    <title> <?php echo $title ?> </title>
 </head>
 <body>
-    <h2>MAIN MAIN</h2>
+    <?php require_once VIEW.$view ?>
 </body>
 </html>
