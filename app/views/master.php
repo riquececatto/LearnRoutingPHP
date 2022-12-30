75aa7eb9cb9e46000062ae301b51cc7fa8a8b709
Henrique Cecatto <rique.cecatto00@gmail.com>
2022-12-30 00:35:31 -0300

    Added styles, home and header views in master.php

--- app/views/master.php
+++ app/views/master.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/assets/css/styles.css">
+
     <title> <?php echo $title ?> </title>
 </head>
 <body>
-    <?php require_once VIEW.$view ?>
+    <?php require_once VIEW . 'partials/header/header.php'; ?>
+    <?php require_once VIEW . $view;?>
 </body>
 </html>
