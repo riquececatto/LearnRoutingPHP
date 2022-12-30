07ff3428e6a067b382110b9abe67f5a1977a3b90
Henrique Cecatto <rique.cecatto00@gmail.com>
2022-12-30 00:34:05 -0300

    Added home and header views

---
+++ app/views/partials/header/header.php
@@ -0,0 +1,14 @@
+<div class="header">
+    <div class="container">
+        <div class="navbar">
+
+
+            <nav class="navbar__menu flex">
+                <li class="navbar__item"><a href="#" class="navbar__link">Text 1</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Text 2</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Text 3</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Text 4</a></li>
+            </nav>
+        </div>
+    </div>
+</div>
