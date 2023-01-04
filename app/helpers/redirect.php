7facee0adde9e1b88b095a933014bb5ebc7feb1f
Henrique Cecatto <rique.cecatto00@gmail.com>
2023-01-04 18:26:01 -0300

    Merged redirect function with flashMessage

--- app/helpers/redirect.php
+++ app/helpers/redirect.php
@@ -4,3 +4,10 @@ function redirect($to)
 {
     return header('Location: '.$to);
 }
+
+function setMessageAndRedirect($index, $message, $redirectTo)
+{
+    setFlash($index, $message);
+    return redirect($redirectTo);
+}
+
